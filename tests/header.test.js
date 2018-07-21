28ad2aa44bbf8be9b0efe0b980afa10a58b61ece
qzy <weelgod@gmail.com>
2018-07-21 14:28:07 +0800

    104. test login

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,22 +1,19 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
-let browser, page;
+
+const Page = require('./helpers/page');
+
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test('header有文字', async () => {
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  const text = await page.getContentsOf('a.brand-logo');
   expect(text).toEqual('Blogster');
 });
 
@@ -27,14 +24,8 @@ test('登录oauth流程', async () => {
 });
 
 test('登陆后显示退出', async () => {
-  const user = await userFactory();
-  const {session, sig} = sessionFactory(user);
+  await page.login();
 
-  await page.setCookie({name: 'session', value: session});
-  await page.setCookie({name: 'session.sig', value: sig});
-  await page.goto('localhost:3000');
-  await page.waitFor('a[href="/auth/logout"]');
-
-  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+  const text = await page.getContentsOf('a[href="/auth/logout"]');
   expect(text).toEqual('Logout');
 });
